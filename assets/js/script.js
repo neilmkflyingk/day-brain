8a7a7859dc196fd412fbdcc7f40d5db078e1a6ef
neilmkflyingk <126199320+neilmkflyingk@users.noreply.github.com>
2023-04-18 19:40:30 -0700

    js setup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+var savedData;
 
 function hourColors() {
   var now = dayjs();
@@ -14,15 +15,32 @@ function hourColors() {
 }
 
 function loadData() {
-
+  savedData = JSON.parse(localStorage.getItem('scheduleEvents'));
+  if (!savedData) {
+    savedData = {
+      hour9: '',
+      hour10: '',
+      hour11: '',
+      hour12: '',
+      hour13: '',
+      hour14: '',
+      hour15: '',
+      hour16: '',
+      hour17: '',
+    };
+  }
 }
 
-function handleSave() {
-  var save = $(event.target);
+function handleSave(event) {
+  var hourRow = $(event.target).parent();
+  var text = hourRow.children('textarea').val();
+  var hour = hourRow.attr('id').split('-')[1];
+  savedData['hour' + hour] = value;
+  localStorage.setItem('scheduleEvents', JSON.stringify(savedData));
 }
 
 $(function() {
-  //loadData();
+  loadData();
   hourColors();
 });
 
